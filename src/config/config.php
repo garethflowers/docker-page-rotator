cf306179f13f34e71c78473b5584bc5f8f45389d
Gareth Flowers <garethflowers@me.com>
2016-10-20 10:57:05 +0100

    Adds `delay` config option

--- src/config/config.php
+++ src/config/config.php
@@ -3,6 +3,8 @@
 return [
     // Name of the page
     'sitename' => 'Page Rotator',
+    // Rotation Delay, in seconds
+    'delay' => 10,
     // List of URL's
     'urls' => [
         'http://test',
