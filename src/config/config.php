55cc79493cbd2e3bb8356c3c71ccdb4f6ec9f69e
Gareth Flowers <garethflowers@users.noreply.github.com>
2016-10-20 10:38:45 +0100

    Adds initial scripts

---
+++ src/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    // Name of the page
+    'sitename' => 'Page Rotator',
+    // List of URL's
+    'urls' => [
+        'http://test',
+    ]
+];
---
+++ src/public/index.php
@@ -0,0 +1,20 @@
+<?php require dirname( __DIR__ ) . '/common/common.php'; ?>
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title><?php echo $config['sitename']; ?></title>
+        <base href="/">
+        <link href="/assets/styles.css" rel="stylesheet">
+    </head>
+
+    <body>
+
+        <iframe id="frame" seamless="seamless" scrolling="no">
+        </iframe>
+
+        <script src="/assets/rotator.php"></script>
+
+    </body>
+
+</html>
