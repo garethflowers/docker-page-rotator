eb5bc4e534d0d05029dc965c5dfd5e1ab51a9c54
Gareth Flowers <garethflowers@me.com>
2016-11-17 12:40:17 +0000

    Add `styles` config parameter
    Allows adding additional CSS styles to the main page.

--- src/public/index.php
+++ src/public/index.php
@@ -1,20 +1,14 @@
 <?php require dirname( __DIR__ ) . '/common/common.php'; ?>
 <!DOCTYPE html>
 <html>
-
     <head>
         <title><?php echo $config['sitename']; ?></title>
         <base href="/">
         <link href="/assets/styles.css" rel="stylesheet">
+        <style><?php echo $config['styles']; ?></style>
     </head>
-
     <body>
-
-        <iframe id="frame" sandbox="allow-scripts allow-same-origin">
-        </iframe>
-
-        <script src="/assets/rotator.php"></script>
-
+        <iframe id="frame" sandbox="allow-scripts allow-same-origin"></iframe>
+        <script src="/assets/rotator.php" async></script>
     </body>
-
 </html>
