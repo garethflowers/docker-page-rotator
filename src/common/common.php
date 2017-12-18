30c02dbd995f5d79ab78d834febf99bd37cd1619
Gareth Flowers <garethflowers@users.noreply.github.com>
2017-12-18 22:40:47 +0000

    Adds demo pages

--- src/common/common.php
+++ src/common/common.php
@@ -4,18 +4,18 @@
 $config = require dirname( __DIR__ ) . '/config/config.php';
 
 // Validate Config
-if (empty($config['sitename'])) {
-	$config['sitename'] = 'Page Rotator';
+if ( empty( $config['sitename'] ) ) {
+    $config['sitename'] = 'Page Rotator';
 }
 
-if (empty($config['delay'])) {
-	$config['delay'] = 10000;
+if ( empty( $config['delay'] ) ) {
+    $config['delay'] = 10000;
 } else {
-	$config['delay'] = ( (int) $config['delay'] ) * 1000;
+    $config['delay'] = ( (int) $config['delay'] ) * 1000;
 }
 
-if (!is_array($config['urls']) || empty($config['urls'])) {
-	$config['urls'] = [ "about:blank" ];
+if ( ! is_array( $config['urls'] ) || empty( $config['urls'] ) ) {
+    $config['urls'] = [ "about:blank" ];
 }
 
 $config['urlList'] = '"' . implode( '", "', $config['urls'] ) . '"';
