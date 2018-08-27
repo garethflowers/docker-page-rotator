b3a8a5655568864ab6d6f46f2474b37d2b7538bf
Gareth Flowers <garethflowers@users.noreply.github.com>
2018-08-27 23:20:15 +0100

    style(html): standardise html formatting

--- src/public/index.php
+++ src/public/index.php
@@ -1,14 +1,17 @@
 <?php require dirname( __DIR__ ) . '/common/common.php'; ?>
 <!DOCTYPE html>
 <html>
-	<head>
-		<title><?php echo $config['sitename']; ?></title>
-		<base href="/">
-		<link href="/assets/styles.css" rel="stylesheet">
-		<script src="/assets/rotator.php" async></script>
-		<style><?php echo $config['styles']; ?></style>
-	</head>
-	<body>
-		<?php echo $config['body']; ?>
-	</body>
+
+<head>
+	<title><?php echo $config['sitename']; ?></title>
+	<base href="/">
+	<link href="/assets/styles.css" rel="stylesheet">
+	<script src="/assets/rotator.php" async></script>
+	<style><?php echo $config['styles']; ?></style>
+</head>
+
+<body>
+	<?php echo $config['body']; ?>
+</body>
+
 </html>
